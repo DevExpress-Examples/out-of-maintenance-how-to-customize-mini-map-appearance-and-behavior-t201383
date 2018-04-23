b01dd3750e032ef6989d8458f198ef85cbd5ffa5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize mini map appearance and behavior
+
+
+This example shows how to customize mini map behavior and appearance. <br />For this assign the <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapMiniMapBehaviortopic">MiniMapBehavior</a> class descendant object to the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMiniMap_Behaviortopic">MiniMap.Behavior</a> property and specify the object's properties. <br />Then Specify the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMiniMap_Alignmenttopic">MiniMap.Alignment</a>, <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMiniMap_Widthtopic">MiniMap.Width</a>, <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMiniMap_Heighttopic">MiniMap.Height</a> and <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMiniMap_ViewportStyletopic">MiniMap.ViewportStyle</a> properties.
+
+<br/>
+
+
---
+++ VB/MiniMapParameters/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports DevExpress.XtraMap
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Namespace MiniMapParameters
+    Partial Public Class Form1
+        Inherits Form
+
+        #Region "#InitEditors"
+        Private behaviors As New List(Of MiniMapBehavior)() From { _
+            New FixedMiniMapBehavior(), _
+            New DynamicMiniMapBehavior() _
+        }
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            cbAlignment.DataSource = System.Enum.GetValues(GetType(MiniMapAlignment))
+            cbBehavior.DataSource = behaviors
+        End Sub
+        #End Region ' #InitEditors
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        #Region "#BehaviorChanged"
+        Private Sub cbBehavior_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles cbBehavior.SelectedIndexChanged
+            Dim behavior As MiniMapBehavior = TryCast(cbBehavior.SelectedValue, MiniMapBehavior)
+            If behavior Is Nothing Then
+                Return
+            End If
+            propertyGrid.SelectedObject = behavior
+
+            If mapControl.MiniMap Is Nothing Then
+                Return
+            End If
+            mapControl.MiniMap.Behavior = behavior
+
+        End Sub
+        #End Region ' #BehaviorChanged
+
+        #Region "#AlignmentChanged"
+        Private Sub cbAlignment_SelectedIndexChanged(ByVal sender As Object, ByVal e As EventArgs) Handles cbAlignment.SelectedIndexChanged
+            Dim alignment As MiniMapAlignment = DirectCast(cbAlignment.SelectedValue, MiniMapAlignment)
+            If mapControl.MiniMap Is Nothing Then
+                Return
+            End If
+            mapControl.MiniMap.Alignment = alignment
+        End Sub
+        #End Region ' #AlignmentChanged
+    End Class
+End Namespace
